51d81f39bb91bf24ec104378775f1c12bd657e4d
Harshita Nagpal <harshita.nagpal005@gmail.com>
2026-09-04 20:28:21 +0530

    Time: 7 ms (49.62%) | Memory: 43.5 MB (18.11%) - LeetSync

---
+++ 409-longest-palindrome/longest-palindrome.java
@@ -0,0 +1,21 @@
+class Solution {
+    public int longestPalindrome(String s) {
+
+        HashMap<Character, Integer> map = new HashMap<>();
+        for (char ch : s.toCharArray()) {
+            map.put(ch, map.getOrDefault(ch, 0) + 1);
+        }
+        int ans = 0;
+        boolean odd = false;
+        for (int freq : map.values()) {
+            ans += (freq / 2) * 2;
+            if (freq % 2 == 1) {
+                odd = true;
+            }
+        }
+        if (odd) {
+            ans++;
+        }
+        return ans;
+    }
+}
